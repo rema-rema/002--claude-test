d93d2e151dd1caf0ccf63197dedd52286989f4bc
rema-rema <68048681+rema-rema@users.noreply.github.com>
2025-08-15 15:28:37 +0000

    Add GET support for direct wake URL access

--- api/wake.js
+++ api/wake.js
@@ -1,5 +1,6 @@
 export default async function handler(req, res) {
-  if (req.method !== 'POST') {
+  // Allow GET requests with wake=true parameter
+  if (req.method !== 'POST' && !(req.method === 'GET' && req.query.wake === 'true')) {
     return res.status(405).json({ error: 'Method not allowed' });
   }
 
